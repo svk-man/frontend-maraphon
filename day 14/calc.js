e78958bfced60b0be83a0cf9fc81fbbd7e73d7ac
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-01 19:12:00 +0300

    refactor calc function

--- day 14/calc.js
+++ day 14/calc.js
@@ -1,6 +1,6 @@
 function calc(operation, operand1, operand2) {
   function isValidOperand(operand) {
-    return operand && typeof(operand) === 'number' && !isNaN(operand);
+    return operand !== undefined && typeof(operand) === 'number' && !isNaN(operand);
   }
 
   let result = '';
@@ -19,24 +19,25 @@ function calc(operation, operand1, operand2) {
         result = operand1 * operand2;
         break;
       case 'div':
-        result = (operand2 === 0 ? 'Error' : operand1 / operand2);
+        result = (operand2 === 0 ? 'Error: division by zero' : operand1 / operand2);
         break;
       case 'mod':
-        result = operand1 % operand2;
+        result = (operand2 === 0 ? 'Error: division by zero' : operand1 % operand2);
         break;
       case 'pow':
         result = operand1 ** operand2;
         break;
       default:
-        result = 'unknown operation';
+        result = 'Error: unknown operation';
     }
   } else {
-    result = 'Error';
+    result = 'Error: not valid operand';
   }
 
   return result;
 }
 
+console.log('Test 0: 0 * 50:', calc('mul', 0, 50));
 console.log('Test 1: 30 + 10:', calc('add', 30, 10));
 console.log('Test 2: 30 - 10:', calc('sub', 30, 10));
 console.log('Test 3: 30 * 10:', calc('mul', 30, 10));
@@ -45,7 +46,8 @@ console.log('Test 5: 30 % 20:', calc('mod', 30, 20));
 console.log('Test 6: 2 ** 5:', calc('pow', 2, 5));
 console.log('Test 7: is unknown operation?:', calc('unk', 2, 5));
 console.log('Test 8: 10 / 0:', calc('div', 10, 0));
-console.log('Test 9: operand2 - undefined:', calc('add', 10));
-console.log('Test 10: operand2 - string:', calc('add', 10, 'string'));
-console.log('Test 11: operand1 and operand2 - string type, but include only numbers:', calc('add', '123', '32'));
-console.log('Test 12: operand1 and operand2 - string type, but include numbers and symbols:', calc('add', '123', '32as'));
+console.log('Test 9: 10 % 0:', calc('mod', 10, 0));
+console.log('Test 10: operand2 - undefined:', calc('add', 10));
+console.log('Test 11: operand2 - string:', calc('add', 10, 'string'));
+console.log('Test 12: operand1 and operand2 - string type, but include only numbers:', calc('add', '123', '32'));
+console.log('Test 13: operand1 and operand2 - string type, but include numbers and symbols:', calc('add', '123', '32as'));
