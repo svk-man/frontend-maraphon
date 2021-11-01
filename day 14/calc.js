cf37605fb3be7bee91561b3cc4e47e34197aab2a
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-01 11:04:02 +0300

    refactor calc function

--- day 14/calc.js
+++ day 14/calc.js
@@ -1,49 +1,51 @@
-function Calc(action, a, b) {
-  if (a === undefined || b === undefined) {
-    return 'Error';
-  }
-
-  a = Number(a);
-  b = Number(b);
-  if (!(typeof a === 'number' && !isNaN(a) && typeof b === 'number' && !isNaN(b))) {
-    return 'Error';
+function calc(operation, operand1, operand2) {
+  function isValidOperand(operand) {
+    return operand && typeof(operand) === 'number' && !isNaN(operand);
   }
 
   let result = '';
-  switch(action) {
-    case 'add':
-      result = a + b;
-      break;
-    case 'sub':
-      result = a - b;
-      break;
-    case 'mul':
-      result = a * b;
-      break;
-    case 'div':
-      result = (b === 0 ? 'Error' : a / b);
-      break;
-    case 'mod':
-      result = a % b;
-      break;
-    case 'pow':
-      result = a ** b;
-      break;
-    default:
-      result = 'unknown operation';
+  operand1 = Number(operand1);
+  operand2 = Number(operand2);
+
+  if (isValidOperand(operand1) && isValidOperand(operand2)) {
+    switch (operation) {
+      case 'add':
+        result = operand1 + operand2;
+        break;
+      case 'sub':
+        result = operand1 - operand2;
+        break;
+      case 'mul':
+        result = operand1 * operand2;
+        break;
+      case 'div':
+        result = (operand2 === 0 ? 'Error' : operand1 / operand2);
+        break;
+      case 'mod':
+        result = operand1 % operand2;
+        break;
+      case 'pow':
+        result = operand1 ** operand2;
+        break;
+      default:
+        result = 'unknown operation';
+    }
+  } else {
+    result = 'Error';
   }
 
   return result;
 }
 
-console.log('Test 1: 30 + 10:', Calc('add', 30, 10));
-console.log('Test 2: 30 - 10:', Calc('sub', 30, 10));
-console.log('Test 3: 30 * 10:', Calc('mul', 30, 10));
-console.log('Test 4: 30 / 10:', Calc('div', 30, 10));
-console.log('Test 5: 30 % 20:', Calc('mod', 30, 20));
-console.log('Test 6: 2 ** 5:', Calc('pow', 2, 5));
-console.log('Test 7: 10 / 0:', Calc('div', 10, 0));
-console.log('Test 8: b - undefined:', Calc('add', 10));
-console.log('Test 9: b - string:', Calc('add', 10, 'string'));
-console.log('Test 10: a, b - string, but numbers:', Calc('add', '123', '32'));
-console.log('Test 11: a, b - string, but numbers and symbols:', Calc('add', '123', '32as'));
+console.log('Test 1: 30 + 10:', calc('add', 30, 10));
+console.log('Test 2: 30 - 10:', calc('sub', 30, 10));
+console.log('Test 3: 30 * 10:', calc('mul', 30, 10));
+console.log('Test 4: 30 / 10:', calc('div', 30, 10));
+console.log('Test 5: 30 % 20:', calc('mod', 30, 20));
+console.log('Test 6: 2 ** 5:', calc('pow', 2, 5));
+console.log('Test 7: is unknown operation?:', calc('unk', 2, 5));
+console.log('Test 8: 10 / 0:', calc('div', 10, 0));
+console.log('Test 9: operand2 - undefined:', calc('add', 10));
+console.log('Test 10: operand2 - string:', calc('add', 10, 'string'));
+console.log('Test 11: operand1 and operand2 - string type, but include only numbers:', calc('add', '123', '32'));
+console.log('Test 12: operand1 and operand2 - string type, but include numbers and symbols:', calc('add', '123', '32as'));
