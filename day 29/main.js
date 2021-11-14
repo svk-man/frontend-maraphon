7d66730325d4bad0b94e77a7c4f51225c5ce81b1
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-15 00:12:56 +0300

    feat: day 29: implemented getting middle age

--- day 29/main.js
+++ day 29/main.js
@@ -337,3 +337,31 @@ for (let key in count) {
 }
 
 console.log();
+
+/*
+Напишите функцию getAverageAge(users), которая принимает массив объектов со свойством age и возвращает средний возраст.
+
+Формула вычисления среднего арифметического значения: (age1 + age2 + ... + ageN) / N.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+*/
+vasya = { name: "Вася", age: 25 };
+petya = { name: "Петя", age: 30 };
+masha = { name: "Маша", age: 29 };
+
+arr = [ vasya, petya, masha ];
+
+function getAverageAge(arr) {
+  return arr.reduce((sumAge, user) => sumAge + user.age, 0) / arr.length;
+}
+
+console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+console.log();
