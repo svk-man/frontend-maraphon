2e0561b35b5d338681066ca1298b51e566dba092
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-14 23:33:28 +0300

    feat: day 29: implemented transforming to an array of names

--- day 29/main.js
+++ day 29/main.js
@@ -177,3 +177,28 @@ powerCalc.addMethod("**", (a, b) => a ** b);
 let result = powerCalc.calculate("2 ** 3");
 console.log(result); // 8
 console.log();
+
+/*
+У вас есть массив объектов user, и в каждом из них есть user.name. Напишите код, который преобразует их в массив имён.
+
+Например:
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = // ... ваш код
+
+alert( names ); // Вася, Петя, Маша
+*/
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [ vasya, petya, masha ];
+
+let names = users.map(item => item.name);
+
+console.log(names);
