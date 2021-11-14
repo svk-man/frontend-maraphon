defa8de65dec82bb3b64284ab24c83435172b14a
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-15 00:03:38 +0300

    feat: day 29: implemented shuffling the array

--- day 29/main.js
+++ day 29/main.js
@@ -278,3 +278,62 @@ console.log(arr[0].name); // Вася
 console.log(arr[1].name); // Маша
 console.log(arr[2].name); // Петя
 console.log();
+
+/*
+Напишите функцию shuffle(array), которая перемешивает (переупорядочивает случайным образом) элементы массива.
+
+Многократные прогоны через shuffle могут привести к разным последовательностям элементов. Например:
+
+let arr = [1, 2, 3];
+
+shuffle(arr);
+// arr = [3, 2, 1]
+
+shuffle(arr);
+// arr = [2, 1, 3]
+
+shuffle(arr);
+// arr = [3, 1, 2]
+// ...
+Все последовательности элементов должны иметь одинаковую вероятность.
+Например, [1,2,3] может быть переупорядочено как [1,2,3] или [1,3,2], или [3,1,2] и т.д., с равной вероятностью каждого случая.
+*/
+console.log();
+function shuffle(arr) {
+  let result = [];
+
+  arr.forEach(item => {
+    Math.round(Math.random()) ? result.push(item) : result.unshift(item);
+  });
+
+  return result;
+}
+
+arr = [1, 2, 3];
+console.log(shuffle(arr));
+console.log(shuffle(arr));
+console.log(shuffle(arr));
+console.log();
+
+// подсчёт вероятности для всех возможных вариантов
+let count = {
+  '123': 0,
+  '132': 0,
+  '213': 0,
+  '231': 0,
+  '321': 0,
+  '312': 0
+};
+
+for (let i = 0; i < 1000000; i++) {
+  let array = [1, 2, 3];
+  array = shuffle(array);
+  count[array.join('')]++;
+}
+
+// показать количество всех возможных вариантов
+for (let key in count) {
+  console.log(`${key}: ${count[key]}`);
+}
+
+console.log();
