7e897993cefb6f2376cec84ab833b30390383bb5
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-11-14 23:43:16 +0300

    feat: day 29: implemented transforming to objects

--- day 29/main.js
+++ day 29/main.js
@@ -202,3 +202,43 @@ let users = [ vasya, petya, masha ];
 let names = users.map(item => item.name);
 
 console.log(names);
+
+/*
+У вас есть массив объектов user, и у каждого из объектов есть name, surname и id.
+
+Напишите код, который создаст ещё один массив объектов с параметрами id и fullName, где fullName – состоит из name и surname.
+
+Например:
+
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = // ... ваш код ...
+
+usersMapped = [
+  { fullName: "Вася Пупкин", id: 1 },
+  { fullName: "Петя Иванов", id: 2 },
+  { fullName: "Маша Петрова", id: 3 }
+]
+
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин
+Итак, на самом деле вам нужно трансформировать один массив объектов в другой. Попробуйте использовать =>. Это небольшая уловка.
+*/
+vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+petya = { name: "Петя", surname: "Иванов", id: 2 };
+masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+users = [ vasya, petya, masha ];
+
+let usersMapped = users.map(item => ({
+  'id': item.id,
+  'fullName': item.name + ' ' + item.surname
+}));
+
+console.log(usersMapped[0].id); // 1
+console.log(usersMapped[0].fullName); // Вася Пупкин
+console.log();
