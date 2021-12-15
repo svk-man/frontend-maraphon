8ff0a6b9b8ab13d0895c96fa77ddf31e7eeac3fe
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2021-12-15 19:58:02 +0300

    feat: day 35: done get average value for array task from codewars

--- day 35/main.js
+++ day 35/main.js
@@ -15,3 +15,13 @@ function buildFun(n){
 for(var i = 0; i < 10; i++){
   console.log(buildFun(10)[i]());
 }
+
+
+function getAverage(marks){
+  const sum = marks.reduce(function(sum, item) { return sum + item; }, 0);
+  return parseInt(sum / marks.length);
+}
+
+console.log(getAverage([2,2,2,2]));
+console.log(getAverage([1,2,3,4,5,]));
+console.log(getAverage([1,1,1,1,1,1,1,2]));
