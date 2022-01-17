914dbae1d232a4a4112f53d18781b19bff5701a6
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-01-17 23:38:42 +0300

    feat: add module for calc

--- day 43/main.js
+++ day 43/main.js
@@ -1,10 +1,4 @@
-const UI_ELEMENTS = {
-  output: document.querySelector('.calc__output'),
-  clearBtn: document.querySelector('.calc__clear-btn'),
-  deleteBtn: document.querySelector('.calc__delete-btn'),
-  numberBtns: document.querySelectorAll('.calc__number-btn'),
-  operatorBtns: document.querySelectorAll('.calc__operator-btn'),
-};
+import { UI_ELEMENTS } from "./view.js";
 
 const OPERATIONS = {
   ADD: 'add',
---
+++ day 43/view.js
@@ -0,0 +1,7 @@
+export const UI_ELEMENTS = {
+  output: document.querySelector('.calc__output'),
+  clearBtn: document.querySelector('.calc__clear-btn'),
+  deleteBtn: document.querySelector('.calc__delete-btn'),
+  numberBtns: document.querySelectorAll('.calc__number-btn'),
+  operatorBtns: document.querySelectorAll('.calc__operator-btn'),
+};
