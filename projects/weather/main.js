a373e25c61f95a8027ca8cc53ba680008120c7a2
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-02-03 23:09:20 +0300

    feat: add catch for promises

--- projects/weather/main.js
+++ projects/weather/main.js
@@ -24,14 +24,21 @@ function changeCity(event) {
         UI_ELEMENTS.WEATHER_NOW_CITY.textContent = cityInfo['name'],
         UI_ELEMENTS.WEATHER_NOW_TEMPERATURE.textContent = Math.round(cityInfo['temperature'])
         UI_ELEMENTS.WEATHER_NOW.style.backgroundImage = `url('${CONFIG.SERVER_ICON_URL}${cityInfo['icon']}@2x.png')`;
-      });
+      })
+      .catch(error => alert('Упс! Что-то пошло не так: ' + error.message));
   }
 }
 
 function getCityInfo(cityName) {
   const url = `${CONFIG.SERVER_URL}?q=${cityName}&appid=${CONFIG.API_KEY}&units=metric`;
   return fetch(url)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} (${response.statusText})`);
+      }
+
+      response.json();
+    })
     .then(cityInfo => {
       function getIcon(cityInfo) {
         return cityInfo.weather[cityInfo.weather.length - 1].icon;
@@ -42,5 +49,6 @@ function getCityInfo(cityName) {
         'temperature': cityInfo.main.temp,
         'icon': getIcon(cityInfo),
       };
-    });
+    })
+    .catch(error => alert('Упс! Что-то пошло не так: ' + error.message));
 }
