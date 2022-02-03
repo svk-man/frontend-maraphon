1079df24e6fb80162add60e592c43aa7b5ba6cc8
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-02-03 18:11:34 +0300

    feat: display weather info for weather now tab

---
+++ projects/weather/main.js
@@ -0,0 +1,46 @@
+const CONFIG = {
+  'SERVER_URL': 'http://api.openweathermap.org/data/2.5/weather',
+  'API_KEY': '',
+  'SERVER_ICON_URL': 'http://openweathermap.org/img/wn/'
+};
+
+const UI_ELEMENTS = {
+  'WEATHER_FORM': document.querySelector('.weather-app__form'),
+  'WEATHER_INPUT': document.querySelector('.weather-app__input'),
+  'WEATHER_NOW': document.querySelector('.weather-now'),
+  'WEATHER_NOW_CITY': document.querySelector('.weather-now__city'),
+  'WEATHER_NOW_TEMPERATURE': document.querySelector('.weather-now__temperature'),
+};
+
+UI_ELEMENTS.WEATHER_FORM.addEventListener('submit', changeCity);
+
+function changeCity(event) {
+  event.preventDefault();
+
+  const cityName = UI_ELEMENTS.WEATHER_INPUT.value;
+  if (cityName) {
+    getCityInfo(cityName)
+      .then(cityInfo => {
+        UI_ELEMENTS.WEATHER_NOW_CITY.textContent = cityInfo['name'],
+        UI_ELEMENTS.WEATHER_NOW_TEMPERATURE.textContent = Math.round(cityInfo['temperature'])
+        UI_ELEMENTS.WEATHER_NOW.style.backgroundImage = `url('${CONFIG.SERVER_ICON_URL}${cityInfo['icon']}@2x.png')`;
+      });
+  }
+}
+
+function getCityInfo(cityName) {
+  const url = `${CONFIG.SERVER_URL}?q=${cityName}&appid=${CONFIG.API_KEY}&units=metric`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(cityInfo => {
+      function getIcon(cityInfo) {
+        return cityInfo.weather[cityInfo.weather.length - 1].icon;
+      }
+
+      return {
+        'name': cityInfo.name,
+        'temperature': cityInfo.main.temp,
+        'icon': getIcon(cityInfo),
+      };
+    });
+}
