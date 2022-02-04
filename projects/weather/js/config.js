f479e388ec2c0e5613842c09c28dc9892e48105b
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-02-04 18:26:23 +0300

    fix: remove api key

--- projects/weather/js/config.js
+++ projects/weather/js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
   'SERVER_URL': 'http://api.openweathermap.org/data/2.5/weather',
-  'API_KEY': '484edaf88f43d42d9059384d04d413b6',
+  'API_KEY': '',
   'SERVER_ICON_URL': 'http://openweathermap.org/img/wn/'
 };
