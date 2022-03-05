324cdc53f750b4b6d0b0717431665b8f259b9823
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-05 16:40:25 +0300

    feat: add error message for input

--- projects/react/genderize/src/components/Input.jsx
+++ projects/react/genderize/src/components/Input.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Input(props) {
-  return <input type="text" placeholder="Name..." name={props.name} />
+  const [isNotValidValue, setIsNotValidValue] = useState(false);
+
+  handleChange = handleChange.bind(this);
+
+  function handleChange(event) {
+    const input = event.target;
+    const inputValue = input.value;
+    setIsNotValidValue(inputValue !== '' && inputValue.trim().length <= 2);
+  }
+
+  return (
+    <div>
+      <input type="text" placeholder="Name..." name={props.name} onChange={handleChange} />
+      {isNotValidValue && <span>Error: Name length is too short</span>}
+    </div>
+  );
 }
 
 export default Input;
