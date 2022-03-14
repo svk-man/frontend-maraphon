0040c552e56d56f641bcdc832db7d0d408702b37
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-14 21:59:30 +0300

    feat: add different greetings

--- day_138/my-react-app/src/main.jsx
+++ day_138/my-react-app/src/main.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const name = 'Иван-царевич';
-const element = <h1>Здравствуй, {name}!</h1>
+const hero1 = {
+  firstName: 'Иван',
+  lastName: 'Царевич',
+};
+
+const hero2 = {
+  firstName: 'Марья',
+  lastName: 'Моревна',
+}
+
+function formatName(hero) {
+  return hero.firstName + '-' + hero.lastName;
+}
+
+function getGreeting(sex) {
+  if (sex) {
+    return <h1>Здравствуй, прекрасный {formatName(hero1)}!</h1>
+  }
+
+  return <h1>Приветствую тебя, прелестная {formatName(hero2)}!</h1>
+}
+
+const element = (
+  <>{getGreeting(0)}
+  <p>Как твои дела?</p></>
+);
 
 ReactDOM.render(
   element,
