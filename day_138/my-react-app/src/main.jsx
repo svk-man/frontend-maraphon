4fd5302f03e3ea2434d349b5677ab017c032dc71
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-14 22:07:54 +0300

    feat: add links

--- day_138/my-react-app/src/main.jsx
+++ day_138/my-react-app/src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom'
 const hero1 = {
   firstName: 'Иван',
   lastName: 'Царевич',
+  link: 'https://ru.wikipedia.org/wiki/Иван-царевич',
 };
 
 const hero2 = {
   firstName: 'Марья',
   lastName: 'Моревна',
+  link: 'https://ru.wikipedia.org/wiki/Марья_Моревна',
 }
 
 function formatName(hero) {
@@ -17,10 +19,10 @@ function formatName(hero) {
 
 function getGreeting(sex) {
   if (sex) {
-    return <h1>Здравствуй, прекрасный {formatName(hero1)}!</h1>
+    return <h1>Здравствуй, прекрасный <a href={hero1.link} target='_blank'>{formatName(hero1)}</a>!</h1>
   }
 
-  return <h1>Приветствую тебя, прелестная {formatName(hero2)}!</h1>
+  return <h1>Приветствую тебя, прелестная <a href={hero2.link} target='_blank'>{formatName(hero2)}</a>!</h1>
 }
 
 const element = (
