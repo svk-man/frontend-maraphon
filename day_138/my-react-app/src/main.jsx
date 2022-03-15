fa2ded0668cca103e7f9fa3a80c5b7d2d02af486
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-15 14:09:26 +0300

    feat: add time component

--- day_138/my-react-app/src/main.jsx
+++ day_138/my-react-app/src/main.jsx
@@ -13,17 +13,21 @@ const hero2 = {
   link: 'https://ru.wikipedia.org/wiki/Марья_Моревна',
 }
 
+function Greeting(props) {
+  const sex = props.sex;
+  if (sex) {
+    return <h1>Здравствуй, прекрасный <a href={hero1.link} target='_blank'>{formatName(hero1)}</a>!</h1>;
+  }
+
+  return <h1>Приветствую тебя, прелестная <a href={hero2.link} target='_blank'>{formatName(hero2)}</a>!</h1>;
+}
+
 function formatName(hero) {
   return hero.firstName + '-' + hero.lastName;
 }
 
-function Greeting(props) {
-  const sex = props.sex;
-  if (sex) {
-    return <h1>Здравствуй, прекрасный <a href={hero1.link} target='_blank'>{formatName(hero1)}</a>!</h1>
-  }
-
-  return <h1>Приветствую тебя, прелестная <a href={hero2.link} target='_blank'>{formatName(hero2)}</a>!</h1>
+function Time() {
+  return <p>Время сейчас такое: {getCurrentTime()}</p>;
 }
 
 function getCurrentTime() {
@@ -34,7 +38,7 @@ function update() {
   const element = (
     <><Greeting sex="1" />
     <p>Как твои дела?</p>
-    <p>Время сейчас такое: {getCurrentTime()}</p></>
+    <Time /></>
   );
 
   ReactDOM.render(
