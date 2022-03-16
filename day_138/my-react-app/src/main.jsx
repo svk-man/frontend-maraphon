f6c48043e82f25043c24b9f8ddad5e3e7c6b41e1
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-16 19:33:56 +0300

    feat: add blog component

--- day_138/my-react-app/src/main.jsx
+++ day_138/my-react-app/src/main.jsx
@@ -96,6 +96,38 @@ function NumberList(props) {
   return <ul>{listItems}</ul>;
 }
 
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  );
+  
+  const content = props.posts.map((post) =>
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  );
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  {id: 1, title: 'Привет, мир', content: 'Добро пожаловать в документацию React!'},
+  {id: 2, title: 'Установка', content: 'React можно установить из npm.'}
+];
+
 function App() {
   return (
     <div>
@@ -107,6 +139,7 @@ function App() {
         text={comment.text}
         date={comment.date} />
       <NumberList numbers={numbers} />
+      <Blog posts={posts} />
     </div>
   );
 }
