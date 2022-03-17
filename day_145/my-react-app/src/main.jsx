419cfdadb45d554f495d87be79ce0ff8cf259f77
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-17 22:20:12 +0300

    feat: add calculation temperature component

--- day_145/my-react-app/src/main.jsx
+++ day_145/my-react-app/src/main.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>Вода закипит.</p>;
+  }
+
+  return <p>Вода не закипит.</p>;
+}
+
+function Calculator() {
+  const [ temperature, setTemperature ] = useState(0);
+
+  function handleChange(event) {
+    setTemperature(event.target.value);
+  }
+
+  return (
+    <fieldset>
+      <legend>Введите температуру в градусах Цельсия:</legend>
+      <input type="number" value={temperature} onChange={handleChange} />
+      <BoilingVerdict celsius={temperature} />
+    </fieldset>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Calculator />,
   document.getElementById('root')
 )
