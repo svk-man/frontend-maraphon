13648d37d3c7e161c66dbea9ea7d12b4dde01b74
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-03-24 16:05:19 +0300

    refactor: fix show todo list

--- projects/todo/view.js
+++ projects/todo/view.js
@@ -1,37 +1,61 @@
 import { tasks, STATUSES, PRIORITIES, addTask, getTask, changeStatus, deleteTask } from "./to do.js";
 
 export const UI_ELEMENTS = {
+  todoList: document.querySelector('.todo__list'),
   highList: document.querySelectorAll('.todo__list')[0],
   lowList: document.querySelectorAll('.todo__list')[1],
 };
 
 export function showTodoList() {
-  clearLists();
+  clearTodoList();
 
-  tasks.forEach(task => {
-    const li = createListItemCheckbox(task);
-    if (task.priority === PRIORITIES.HIGH) {
-      UI_ELEMENTS.highList.append(li);
-    } else {
-      UI_ELEMENTS.lowList.append(li);
+  const sortedTasks = tasks.sort(compareTasks);
+  let lastTaskPriority = null;
+  sortedTasks.forEach(task => {
+    if (task.priority != lastTaskPriority) {
+      UI_ELEMENTS.todoList.append(createListItemCategory(task.priority));
+      UI_ELEMENTS.todoList.append(createListItemAddTask(`Add ${task.priority === PRIORITIES.HIGH ? 'important ' : ''}task`));
     }
+
+    UI_ELEMENTS.todoList.append(createListItemTask(task));
+
+    lastTaskPriority = task.priority;
   });
 }
 
-function clearLists() {
-  UI_ELEMENTS.highList.textContent = '';
-  UI_ELEMENTS.lowList.textContent = '';
-
-  UI_ELEMENTS.highList.append(createListItemInput('Add important task'));
-  UI_ELEMENTS.lowList.append(createListItemInput('Add task'));
+function compareTasks(task1, task2) {
+  if ((task1.priority === PRIORITIES.HIGH && task2.priority === PRIORITIES.LOW) || task1.id < task2.id) {
+    return -1;
+  } else if ((task1.priority === PRIORITIES.LOW && task2.priority === PRIORITIES.HIGH) || task2.id < task1.id) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
 
-function createListItemInput(placeholder) {
+function clearTodoList() {
+  UI_ELEMENTS.todoList.textContent = '';
+}
+
+function createListItemCategory(category) {
+  const li = document.createElement('li');
+  li.classList.add('todo__item');
+
+  const span = document.createElement('span');
+  span.textContent = category;
+  span.classList.add('todo__item-category');
+
+  li.append(span);
+
+  return li;
+}
+
+function createListItemAddTask(placeholder) {
   const li = document.createElement('li');
   li.classList.add('todo__item');
 
   const form = document.createElement('form');
-  form.classList.add('todo__item-form');
+  form.classList.add('todo__item-add-task');
 
   const inputText = document.createElement('input');
   inputText.classList.add('todo__item-input');
@@ -52,36 +76,41 @@ function createListItemInput(placeholder) {
   return li;
 }
 
-function createListItemCheckbox(task) {
-  const id = `todo-${task.priority.toLowerCase()}-item-${task.id}`;
+function createListItemTask(task) {
+  const itemId = `todo-item-${task.id}`;
 
   const li = document.createElement('li');
   li.classList.add('todo__item');
   li.dataset.id = task.id;
 
+  const div = document.createElement('div');
+  div.classList.add('todo__item-task');
+
   const input = document.createElement('input');
   input.classList.add('todo__item-checkbox');
   input.type = 'checkbox';
-  input.name = id;
-  input.id = id;
+  input.name = itemId;
+  input.id = itemId;
   input.addEventListener('change', changeListItemCheckboxStatus)
   if (task.status === STATUSES.DONE) {
     li.classList.add('todo__item--done');
     input.checked = true;
   }
 
-  li.append(input);
+  div.append(input);
 
   const label = document.createElement('label');
   label.classList.add('todo__item-checkbox-label');
-  label.htmlFor = id;
+  label.htmlFor = itemId;
   label.textContent = task.name;
-  li.append(label);
+  div.append(label);
 
   const button = document.createElement('button');
   button.classList.add('todo__item-btn', 'todo__item-btn--delete');
   button.addEventListener('click', deleteListItemCheckbox)
-  li.append(button);
+  div.append(button);
+
+  li.append(div);
 
   return li;
 }
