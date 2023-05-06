604d99babd42ec0de70d0a65b0ee8c914ddd3ad7
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-05-06 11:56:02 +0300

    feat: filter unique array elements

---
+++ days/day 89/script.js
@@ -0,0 +1,34 @@
+// Задача - Фильтрация уникальных элементов массива
+function unique(arr) {
+  const set = new Set(arr);
+
+	return Array.from(set);
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log( unique(values) ); // Hare,Krishna,:-O
+
+
+// Задача - Отфильтруйте анаграммы
+function aclean(arr) {
+	const map = new Map();
+
+	arr.forEach(item => {
+		const getKey = (item) => {
+			return item
+				.toLowerCase()
+				.split("").sort().join("");
+		}
+
+		map.set(getKey(item), item);
+	});
+
+	return Array.from(map.values());
+}
+
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+
+console.log( aclean(arr) );
