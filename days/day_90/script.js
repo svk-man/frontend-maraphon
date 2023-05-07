9ac6397a35b326bd9165a8526122318bd00f0b63
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2023-05-07 12:02:36 +0300

    feat: done days 90 tasks

---
+++ days/day_90/script.js
@@ -0,0 +1,30 @@
+// Сумма свойств объекта
+function sumSalaries(salaries) {
+	let sum = 0;
+
+	for (let salary of Object.values(salaries)) {
+		sum += salary;
+	}
+
+	return sum;
+}
+
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+
+console.log( sumSalaries(salaries) ); // 650
+
+// Подсчёт количества свойств объекта
+function count(obj) {
+	return Object.keys(obj).length;
+}
+
+let user = {
+  name: 'John',
+  age: 30
+};
+
+console.log( count(user) ); // 2
